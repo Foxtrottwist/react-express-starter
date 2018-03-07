229908b1c05a98d5e65e008a649e1bd192b59ba1
Law Horne <foxtrottwist@ymail.com>
2018-03-07 12:47:22 -0500

    Render basic timer

--- src/client/App/Timers/Timer.jsx
+++ src/client/App/Timers/Timer.jsx
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import format from 'date-fns/format';
+import differenceInMilliseconds from 'date-fns/difference_in_milliseconds';
+import differenceInHours from 'date-fns/difference_in_hours';
 
 import { Title, Box } from '../utils/sharedStyles';
 
 class Timer extends Component {
+  /* eslint-disable */
   state = {
-    start: '',
     duration: 0,
     time: '',
+  };
+  /* eslint-enable */
+
+  componentDidMount() {
+    this.timer();
+  }
+
+  timer = () => {
+    const start = new Date();
+
+    setInterval(() => {
+      const now = new Date();
+      const duration = differenceInMilliseconds(now, start);
+      const hours = differenceInHours(now, start);
+      const minutesSecondsMilliseconds = format(duration, 'mm:ss');
+      const time =
+        hours < 10
+          ? `0${hours}:${minutesSecondsMilliseconds}`
+          : `${hours}:${minutesSecondsMilliseconds}`;
+      this.setState(() => ({ duration }));
+      this.setState(() => ({ time }));
+    }, 100);
   };
 
   render() {
     return (
       <Box>
         <Title>{this.props.name}</Title>
+        <Title>{this.state.time}</Title>
       </Box>
     );
   }
