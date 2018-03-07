ec203a4ba415c3c1c9784cf041d2a57f28bbbc49
Law Horne <foxtrottwist@ymail.com>
2018-03-07 17:33:32 -0500

    Start and reset

--- src/client/App/Timers/Timer.jsx
+++ src/client/App/Timers/Timer.jsx
@@ -3,28 +3,31 @@ import format from 'date-fns/format';
 import differenceInMilliseconds from 'date-fns/difference_in_milliseconds';
 import differenceInHours from 'date-fns/difference_in_hours';
 
-import { Title, Box } from '../utils/sharedStyles';
+import { Title, Box, Button } from '../utils/sharedStyles';
 
 class Timer extends Component {
   /* eslint-disable */
   state = {
     duration: 0,
-    time: '',
+    time: '00:00:00',
   };
   /* eslint-enable */
 
-  componentDidMount() {
-    this.timer();
-  }
+  componentWillUnmount() {}
 
-  timer = () => {
+  timer = null;
+
+  startTimer = () => {
+    if (this.timer) return;
+
     const start = new Date();
 
-    setInterval(() => {
+    this.timer = setInterval(() => {
       const now = new Date();
       const duration = differenceInMilliseconds(now, start);
       const hours = differenceInHours(now, start);
       const minutesSecondsMilliseconds = format(duration, 'mm:ss');
+      // formatting for the rendered timer
       const time =
         hours < 10
           ? `0${hours}:${minutesSecondsMilliseconds}`
@@ -34,11 +37,25 @@ class Timer extends Component {
     }, 100);
   };
 
+  pause = () => {
+    clearInterval(this.timer);
+    this.timer = null;
+  };
+
+  reset = () => {
+    clearInterval(this.timer);
+    this.timer = null;
+    this.setState(() => ({ time: '00:00:00', duration: 0 }));
+  };
+
   render() {
     return (
       <Box>
         <Title>{this.props.name}</Title>
         <Title>{this.state.time}</Title>
+        <Button onClick={this.startTimer}>Start</Button>
+        <Button onClick={this.pause}>Pause</Button>
+        <Button onClick={this.reset}>Reset</Button>
       </Box>
     );
   }
