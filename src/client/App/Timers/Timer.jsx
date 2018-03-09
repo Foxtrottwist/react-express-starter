f7369fb87af4a51fe9d3b5bb8a17b33c57243cb8
Law Horne <foxtrottwist@ymail.com>
2018-03-09 11:10:45 -0500

    Working pause feature

--- src/client/App/Timers/Timer.jsx
+++ src/client/App/Timers/Timer.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import format from 'date-fns/format';
 import differenceInMilliseconds from 'date-fns/difference_in_milliseconds';
-import differenceInHours from 'date-fns/difference_in_hours';
 
 import { Title, Box, Button } from '../utils/sharedStyles';
 
 class Timer extends Component {
   state = {
     timer: null,
+    offset: null,
+    duration: 0,
     time: '00:00:00',
     start: '',
   };
@@ -19,25 +20,48 @@ class Timer extends Component {
   startTimer = () => {
     if (this.state.timer) return;
 
-    const start = new Date();
+    this.setState(() => ({ offset: Date.now() }));
 
     const timer = setInterval(() => {
-      const now = new Date();
+      const now = Date.now();
+      const { offset } = this.state;
 
-      const duration = differenceInMilliseconds(now, start);
-      const hours = differenceInHours(now, start);
+      // calculate the total duration on the timer
+      const duration = this.state.duration + differenceInMilliseconds(now, offset);
+
+      // formating the output of the timer
+      const hours = Math.floor(duration / 1000 / 3600);
       const minutesSeconds = format(duration, 'mm:ss');
       const time = hours < 10 ? `0${hours}:${minutesSeconds}` : `${hours}:${minutesSeconds}`;
 
-      this.setState(() => ({ time }));
-    }, 100);
+      // updating state with duration and offset to allow for pausing, time renders the output
+      this.setState(() => ({ duration, time, offset: now }));
 
-    this.setState(() => ({ timer, start: format(new Date(), 'dddd MMMM DD, HH:mm') }));
+      // eslint-disable-next-line
+      console.log(this.state);
+    }, 300);
+
+    // if the start time has already been documented, don't overwrite
+    if (!this.state.start) {
+      this.setState(() => ({ timer, start: format(new Date(), 'dddd MMMM DD, HH:mm') }));
+    }
+    this.setState(() => ({ timer }));
+  };
+
+  pause = () => {
+    clearInterval(this.state.timer);
+    this.setState(() => ({ timer: null }));
   };
 
   reset = () => {
     clearInterval(this.state.timer);
-    this.setState(() => ({ timer: null, time: '00:00:00' }));
+    this.setState(() => ({
+      timer: null,
+      offset: null,
+      duration: 0,
+      time: '00:00:00',
+      start: '',
+    }));
   };
 
   render() {
@@ -46,6 +70,7 @@ class Timer extends Component {
         <Title>{this.props.name}</Title>
         <Title>{this.state.time}</Title>
         <Button onClick={this.startTimer}>Start</Button>
+        <Button onClick={this.pause}>Pause</Button>
         <Button onClick={this.reset}>Reset</Button>
       </Box>
     );
